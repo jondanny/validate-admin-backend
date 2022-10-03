3a063da33847376c314d892975c2f316e276defa
jondanny <jdnguyen928@gmail.com>
2022-10-03 19:03:34 +0500

    Fixed Controller module

--- src/ticket-provider-api-token/ticket-provider-api-token.module.ts
+++ src/ticket-provider-api-token/ticket-provider-api-token.module.ts
@@ -3,14 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TicketProviderModule } from '@src/ticket-provider/ticket-provider.module';
 import { TicketProviderValidator } from '@src/ticket-provider/ticket-provider.validator';
 import { TicketProviderExistsValidator } from '@src/ticket-provider/validators/ticket-provider-exists.validator';
-import { TicketProviderApiTokenApiTokenController } from './ticket-provider-api-token.controller';
+import { TicketProviderApiTokenController } from './ticket-provider-api-token.controller';
 import { TicketProviderApiToken } from './ticket-provider-api-token.entity';
 import { TicketProviderApiTokenRepository } from './ticket-provider-api-token.repository';
 import { TicketProviderApiTokenService } from './ticket-provider-api-token.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([TicketProviderApiToken]), TicketProviderModule],
-  controllers: [TicketProviderApiTokenApiTokenController],
+  controllers: [TicketProviderApiTokenController],
   providers: [
     TicketProviderApiTokenService,
     TicketProviderApiTokenRepository,
